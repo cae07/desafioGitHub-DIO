cef2152f1ef560a8bd07af175100ef369eb9f9a2
Cae <eduardokae01@hotmail.com>
2021-10-28 13:06:18 -0300

    add placar e funcionalidade

--- jogo-dino/script.js
+++ jogo-dino/script.js
@@ -1,5 +1,6 @@
 const sonicRunning = document.querySelector('.sonic'); // seleciona o sonic
 const background = document.querySelector('.background');
+const placar = document.querySelector('.placar');
 let isJumping = false; // verifica se está pulando
 let position = 0; // posição do sonic
 
@@ -33,35 +34,36 @@ const jump = () => { // função que lida com os pulos
 }
 
 const createObstacle = () => {
-  let obstaclePosition = 1250; // define posição inicial do obstaculo
   let randomTime = Math.random() * 6000; //randoniza o tempo que o obstaculo reaparece
-  const resetButton = document.createElement('button'); // cria botao reset
-  resetButton.classList.add('reset-button');
-  resetButton.innerText = 'RESET';
+  let obstaclePosition = 1250; // define posição inicial do obstaculo
+  let pontos = 0;
 
   const obstacle = document.createElement('div'); // cria o obstaculo
   obstacle.classList.add('sonicObstacle');
   obstacle.style.left =  `1250`;
   background.appendChild(obstacle);
 
-  let leftInterval = setInterval(() => { // lida com animaçao do obstaculo 
+  let leftInterval = setInterval(() => { // lida com animaçao do obstaculo
     obstaclePosition -= 10;
     obstacle.style.left = `${obstaclePosition}px`;
 
     if (obstaclePosition < -60) { // condiçao para resetar obstaculo e validar q sonic não bateu nele
       clearInterval(leftInterval);
       background.removeChild(obstacle);
+      pontos += 100;
+      placar.innerText = Number(placar.innerText) + 100;
     }
     else if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60) { // lida com sonic bater no obstaculo
       clearInterval(leftInterval);
-      document.body.innerText = 'GAME OVER';
-      background.appendChild(resetButton);
+      background.removeChild(obstacle);
+      document.body.innerText = `Game Over - ${placar.innerText} pontos`;
     }
     else {
       obstaclePosition -= 10;
       obstacle.style.left = `${obstaclePosition}px`;
     };
   }, 25)
+
 
   setTimeout(createObstacle, randomTime);
 }
